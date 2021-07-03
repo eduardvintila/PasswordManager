52cb00370cddc14203908dbd72fd4864e7010e7a
Eduard Vintila <eduard.vintila47@gmail.com>
2021-07-03 15:53:32 +0300

    Add "Entry" class + build dependencies for MVVM and SQLCipher

---
+++ app/src/main/java/com/example/passmanager/Entry.java
@@ -0,0 +1,60 @@
+package com.example.passmanager;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+/**
+ * Entry in the Password Manager.
+ *
+ * <p>This class represents an entry which stores the authentication credentials and other information
+ *    about a service (an app or a website).
+ * </p>
+ */
+@Entity(tableName = "Entry")
+public class Entry {
+
+    /**
+     * Entry number in the Password Manager.
+     */
+    @PrimaryKey
+    @ColumnInfo(name = "entryNo")
+    public int entryNo;
+
+    /**
+     * Name of the entry, typically the name of the website/app.
+     */
+    @ColumnInfo(name = "entryName")
+    public String entryName;
+
+    /**
+     * Description of the entry.
+     */
+    @ColumnInfo(name = "entryDescription")
+    public String entryDescription;
+
+    /**
+     * Link/URL/URI of the service.
+     */
+    @ColumnInfo(name = "serviceLink")
+    public String serviceLink;
+
+    /**
+     * User ID for authentication (it can be an e-mail or a username).
+     */
+    @ColumnInfo(name = "userId")
+    public String userId;
+
+    /**
+     * User password for authentication, stored in an encrypted form by using a salt.
+     */
+    @ColumnInfo(name = "userPassword")
+    public String userPassword;
+
+    /**
+     * Salt generated for encrypting/decrypting the user password.
+     */
+    @ColumnInfo(name = "passwordSalt")
+    public String passwordSalt;
+
+}
